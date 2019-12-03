36307ebbd7b41dc3675d66ec133e7d45795f6891
admin <hadi.vsh@gmail.com>
2019-12-03 14:32:07 +0330

    Chapter2-Introduction to functions

--- learncpp/C++ Basics Functions and Files/Introduction_to_functions/main.cpp
+++ learncpp/C++ Basics Functions and Files/Introduction_to_functions/main.cpp
@@ -1,8 +1,28 @@
 #include <QCoreApplication>
+#include <QtDebug>
+#include <iostream>
+#include <QString>
+using namespace std;
 
+
+void doPrint()
+{
+    cout<< "Test print";
+}
+
+int factorial(int i){
+    return i!=0?i*factorial(i-1):1;
+
+}
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    int i;
+    cin>>i;
+    //    doPrint();
+    cout<< to_string(i)+"! = "<<factorial(i)<<endl;
 
-    return a.exec();
+    //return a.exec();
+    // For exit with Enter Key
+    return 0;
 }
